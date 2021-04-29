f59bb53fc5f0dd8ad939eede09ce944a5b5e3b4c
Vadim Zarifullin <65528175+Zarvad23@users.noreply.github.com>
2021-04-29 07:12:21 +0300

    Create copy.py

---
+++ copy.py
@@ -0,0 +1,28 @@
+import shutil
+import keyboard
+import os
+import time
+
+
+game = "C:\Games\FTLAdvancedEdition\FTL Advanced Edition\FTL Advanced Edition\FTLGame.exe"
+prof_g = r'C:\Users\Student\Documents\My Games\FasterThanLight\ae_prof.sav'#Путь до сохранения профиля игры
+prof_s = r'C:\Users\Student\Documents\My Games\Save\ae_prof.sav'#Путь до сохранения профиля игры в папке Save
+save_g = r'C:\Users\Student\Documents\My Games\FasterThanLight\continue.sav'#Путь до сохранения игры
+save_s = r'C:\Users\Student\Documents\My Games\Save\continue.sav'#Путь до сохранения игры в папке Save
+
+while True:
+	if keyboard.is_pressed('c'):
+		os.system("TASKKILL /F /IM FTLGame.exe")
+		time.sleep(2)
+		shutil.copy(prof_g, prof_s)
+		shutil.copy(save_g, save_s)
+
+	elif keyboard.is_pressed('v'):
+		os.system("TASKKILL /F /IM FTLGame.exe")
+		time.sleep(2)
+		shutil.copy(prof_s, prof_g)
+		shutil.copy(save_s, save_g)
+		time.sleep(2)
+		os.startfile(game)
+	elif keyboard.is_pressed('esc'):
+		break
