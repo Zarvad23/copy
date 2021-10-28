1ae4e6a51a1aeec3a5bb65e8fb35190e84b54417
Zarvad23 <zarifullinvadim@mail.ru>
2021-10-28 14:27:48 +0500

    56

--- copy.py
+++ copy.py
@@ -4,31 +4,31 @@ import os
 import time
 
 
-game = "C:\Games\FTLAdvancedEdition\FTL Advanced Edition\FTL Advanced Edition\FTLGame.exe"
-prof_g = 'C:\Users\Student\Documents\My Games\FasterThanLight\ae_prof.sav'#Путь до сохранения профиля игры
-prof_s = rr'C:\Users\Student\Documents\My Games\Save\ae_prof.sav'#Путь до сохранения профиля игры в папке Save
-save_g = r'C:\Users\Student\Documents\My Games\FasterThanLight\continue.sav'#Путь до сохранения игры
-save_s = r'C:\Users\Student\Documents\My Games\Save\continue.sav'#Путь до сохранения игры в папке Save
+game = r'D:\Games\FTL Advanced Edition\FTLGame.exe'
+prof_g = r'D:\Documents\My Games\FasterThanLight\ae_prof.sav'#Путь до сохранения профиля игры
+prof_s = r'D:\Desktop\Save\ae_prof.sav'#Путь до сохранения профиля игры в папке Save
+save_g = r'D:\Documents\My Games\FasterThanLight\continue.sav'#Путь до сохранения игры
+save_s = r'D:\Desktop\Save\continue.sav'#Путь до сохранения игры в папке Save
 directory = r'C:\Users\Student\Desktop\Saves'#ут указываешь путь до папки в которой будут хранится папки с сейвами
 
 
 while True:
 	time.sleep(0.1)
-	if keyboard.is_pressed('c'):
+	if keyboard.is_pressed('shift+c'):
 		os.system("TASKKILL /F /IM FTLGame.exe")
 		time.sleep(2)
 		shutil.copy(prof_g, prof_s)
 		shutil.copy(save_g, save_s)
-	elif keyboard.is_pressed('`'):
+	elif keyboard.is_pressed('shift+`'):
 		name = input('Введите название папки')
 		fullname = os.path.join(directory, name)
 		os.mkdir(fullname)
-	elif keyboard.is_pressed('v'):
+	elif keyboard.is_pressed('shift+v'):
 		os.system("TASKKILL /F /IM FTLGame.exe")
 		time.sleep(2)
 		shutil.copy(prof_s, prof_g)
 		shutil.copy(save_s, save_g)
 		time.sleep(2)
 		os.startfile(game)
-	elif keyboard.is_pressed('esc'):
+	elif keyboard.is_pressed('shift+esc'):
 		break
